4e4d12e8f37b2e4686a9b8b0980c94b88d1292a4
Owlz <whootandahalf@gmail.com>
2017-02-12 11:34:07 -0500

    Updates

--- extract/handlers/__init__.py
+++ extract/handlers/__init__.py
@@ -1,6 +1,3 @@
-import logging
-
-logger = logging.getLogger("extract.handlers")
 
 class handleBaseClass:
     """Extend this class to implement custom extract handlers"""
@@ -8,6 +5,30 @@ class handleBaseClass:
     def __init__(self,config):
         self.config = config
 
+        # Store hash of the original file so we don't delete the wrong things
+        with open(config['fileName'],"rb") as f:
+            self.sha256 = hashlib.sha256(f.read()).digest()
+
     def extract(self):
-        logger.warn("Extract isn't implemented.")
+        config = self.config
+
+        # Do we need to remove the file?
+        if config['rm']:
+
+            # Does it still exist and has it not changed?
+            if os.path.exists(config['fileName']) and self._hashNotChanged():
+                logger.debug("Removing original file")
+                os.remove(config['fileName']) 
+
+            
+    def _hashNotChanged(self):
+        """Make sure the file hash hasn't changed""" 
     
+        with open(self.config['fileName'],"rb") as f:
+            return self.sha256 == hashlib.sha256(f.read()).digest()
+
+import logging
+import hashlib
+import os
+
+logger = logging.getLogger("extract.handlers")
--- extract/handlers/zip/__init__.py
+++ extract/handlers/zip/__init__.py
@@ -4,6 +4,26 @@ class handle(handleBaseClass):
 
     def extract(self):
         # TODO: Add super() call for this
+        config = self.config
 
-        pass
+        # Open it up
+        z = zipfile.ZipFile(config['fileName'])
 
+        logger.info("Extracting the following files: {0}".format([x.filename for x in z.filelist]))
+
+        # Find the base directory of the file
+        directory = os.path.dirname(os.path.abspath(config['fileName']))
+        
+        # Do the actual extraction
+        z.extractall(path=directory)
+
+        # Call parent handler
+        handleBaseClass.extract(self)
+        
+        
+
+import zipfile
+import logging
+import os
+
+logger = logging.getLogger('extract.handlers.zip')
