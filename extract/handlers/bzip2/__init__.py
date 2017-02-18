b07165b988120b1044900f9e469d75cffac61fb2
Owlz <whootandahalf@gmail.com>
2017-02-18 14:45:20 -0800

    Adding bzip2 support

---
+++ extract/handlers/bzip2/__init__.py
@@ -0,0 +1,27 @@
+from extract.handlers import handleBaseClass
+
+class handle(handleBaseClass):
+
+    def extract(self):
+        config = self.config
+
+        # Open it up
+        b = bz2.BZ2File(config['fileName'])
+
+        # Find the base directory of the file
+        directory = os.path.dirname(os.path.abspath(config['fileName']))
+        
+        # Do the actual extraction
+        # TODO: Maybe we wanna buffer this...
+        with open(config['fileName'] + "_extracted","wb") as f:
+            f.write(b.read())
+
+        # Call parent handler
+        handleBaseClass.extract(self)
+        
+
+import bz2        
+import logging
+import os
+
+logger = logging.getLogger('extract.handlers.bzip2')
--- extract/handlers_list.py
+++ extract/handlers_list.py
@@ -2,6 +2,7 @@ import handlers.zip
 import handlers.rar
 import handlers.tar
 import handlers.gzip
+import handlers.bzip2
 
 # Dictionary of lists of objects to use to unpack
 handlers = {
@@ -9,4 +10,5 @@ handlers = {
     'application/x-rar': [handlers.rar],
     'application/x-tar': [handlers.tar],
     'application/gzip' : [handlers.gzip],
+    'application/x-bzip2': [handlers.bzip2],
 }
