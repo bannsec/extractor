f5d4e49022d9521cc42268f41a0adad25c965a73
Owlz <whootandahalf@gmail.com>
2017-02-18 14:41:10 -0800

    Adding gzip support

--- extract/handlers_list.py
+++ extract/handlers_list.py
@@ -1,10 +1,12 @@
 import handlers.zip
 import handlers.rar
 import handlers.tar
+import handlers.gzip
 
 # Dictionary of lists of objects to use to unpack
 handlers = {
-    'application/zip': [handlers.zip],
+    'application/zip'  : [handlers.zip],
     'application/x-rar': [handlers.rar],
     'application/x-tar': [handlers.tar],
+    'application/gzip' : [handlers.gzip],
 }
