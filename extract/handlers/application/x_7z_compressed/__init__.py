029e6daf77da47e27b93373045f5613e188a48a7
Owlz <whootandahalf@gmail.com>
2017-02-20 18:47:25 -0500

    7z support

---
+++ extract/handlers/application/x_7z_compressed/__init__.py
@@ -0,0 +1,27 @@
+from extract.handlers import handleBaseClass
+
+class handle(handleBaseClass):
+
+    def extract(self):
+        config = self.config
+
+        # Find the base directory of the file
+        directory = os.path.dirname(os.path.abspath(config['fileName']))
+
+        # Default to extracting to the same directory
+        os.chdir(directory)
+        
+        # Do the actual extraction
+        with open(config['fileName'],"rb") as f:                                            
+            for entry in libarchive.public.memory_pour(f.read()):
+                pass
+
+        # Call parent handler
+        handleBaseClass.extract(self)
+        
+
+import logging
+import os
+import libarchive.public
+
+logger = logging.getLogger('extract.handlers.application.x_7z_compressed')
