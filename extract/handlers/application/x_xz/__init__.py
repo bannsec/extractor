850f0bd2c1ff0a60eee2604f154ecb38abb8128d
Owlz <whootandahalf@gmail.com>
2017-02-20 16:13:44 -0500

    Cleaning up xz output

--- extract/handlers/application/x_xz/__init__.py
+++ extract/handlers/application/x_xz/__init__.py
@@ -10,9 +10,15 @@ class handle(handleBaseClass):
 
         # Find the base directory of the file
         directory = os.path.dirname(os.path.abspath(config['fileName']))
+
+        # Determine output file name
+        if config['fileName'].endswith(".xz"):
+            outFile = config['fileName'][:-3]
+        else:
+            outFile = config['fileName'] + "_extracted"
         
         # Do the actual extraction
-        with open(config['fileName'] + "_extracted","wb") as f:
+        with open(outFile,"wb") as f:
             f.write(l.read())
 
         # Call parent handler
